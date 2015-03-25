b4b9daa1513a03347b2f3d1b2a0d00d7970e73c5
Jianfeng <ginfungchan@gmail.com>
2015-03-24 23:30:11 -0400

    vector(x,y)

--- src/physics/Vec2D.java
+++ src/physics/Vec2D.java
@@ -2,59 +2,59 @@ package physics;
 
 public class Vec2D {
 	public float x;
-	public float z;
+	public float y;
 
-	public Vec2D(float x, float z) {
+	public Vec2D(float x, float y) {
 		this.x = x;
-		this.z = z;
+		this.y = y;
 	}
 
 	public void plusEqual(Vec2D increment) {
 		this.x += increment.x;
-		this.z += increment.z;
+		this.y += increment.y;
 	}
 
 	public Vec2D plus(Vec2D b) {
-		return new Vec2D(this.x + b.x, this.z + b.z);
+		return new Vec2D(this.x + b.x, this.y + b.y);
 	}
 
 	public void minusEqual(Vec2D substraction) {
 		this.x -= substraction.x;
-		this.z -= substraction.z;
+		this.y -= substraction.y;
 	}
 
 	public Vec2D minus(Vec2D b) {
-		return new Vec2D(this.x - b.x, this.z - b.z);
+		return new Vec2D(this.x - b.x, this.y - b.y);
 	}
 
 	public float getLength() {
-		return (float)Math.sqrt((x * x + z * z));
+		return (float)Math.sqrt((x * x + y * y));
 	}
 
 	public void normalize() {
 		float l = this.getLength();
 		this.x /= l;
-		this.z /= l;
+		this.y /= l;
 	}
 	
 	public Vec2D getUnitVec(){
 		float l = this.getLength();
-		return new Vec2D(x/l,z/l);
+		return new Vec2D(x/l,y/l);
 	}
 
 	public Vec2D scale(float s) {
-		return new Vec2D(s * x,  s * z);
+		return new Vec2D(s * x,  s * y);
 	}
 
 	public void truncate(float maxLength) {
 		this.normalize();
 		Vec2D e = this.scale(maxLength);
 		this.x = e.x;
-		this.z = e.z;
+		this.y = e.y;
 	}
 
 	public String toString() {
-		return "(" + x + "," + z + ")";
+		return "(" + x + "," + y + ")";
 	}
 
 	public static void main(String[] args){
