ef8e8b974e4695222079e369b188239a5d43bdc9
Ray <zhirui1992@gmail.com>
2015-03-27 09:51:06 -0400

    for pull

--- src/behavior/Attack.java
+++ src/behavior/Attack.java
@@ -1,6 +1,25 @@
 package behavior;
 
+import main.Config;
+import physics.Accle;
+import environment.Boid;
+
 public class Attack {
 	
+	Accle a = new Accle(0,0);
+	
+	public Attack(Boid b1,Boid b2)
+	{
+
+			//Steering behavior
+			a=(Accle) b1.pos.minus(b2.pos);
+			//clip velocity
+			if(a.getLength()>Config.MAX_LINACC)
+			{
+				a.normalize();;
+				a.multiply(Config.MAX_LINACC);
+			}
+								
+	}
 
 }
