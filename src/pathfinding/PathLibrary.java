ee6d21250a763ec412f561e2ccbc3d03eb51fa36
Yao <Yao@WIN-G7OPLOEHB8C>
2015-03-29 17:47:58 -0400

    Debug

--- src/pathfinding/PathLibrary.java
+++ src/pathfinding/PathLibrary.java
@@ -12,10 +12,12 @@ public class PathLibrary {
 		Graph graph = Main.getGraph();
 		int start = World.quantize(pos);
 		int end = World.quantize(targetPos);
-		
+		//System.out.println("Quantize result: startNode: "+start+", endNode:"+end);
 		if(start == end) return targetPos;
+		//System.out.print("calculating path...");
 
 		List<Integer> path = PathFinding.AStar(graph, start, end);
+		//System.out.println(path);
 		return graph.getNodePos(path.get(0));
 	}
 
