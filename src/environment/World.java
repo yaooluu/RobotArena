41a37fcdb90cb45ec9bc8d6908d7037d594de70b
Jianfeng <ginfungchan@gmail.com>
2015-03-29 16:18:34 -0400

    generate Dirichlet Graph

--- src/environment/World.java
+++ src/environment/World.java
@@ -2,86 +2,133 @@ package environment;
 
 import main.Config;
 import pathfinding.Graph;
+import physics.Vec2D;
 import processing.core.PApplet;
 import processing.core.PImage;
 
 public class World {
+	private static int width = Config.SCREEN_WIDTH;
+	private static int height = Config.SCREEN_HEIGHT;
 
-	private static float tileSize=Config.TILE_SIZE;
-	private static int width=Config.SCREEN_WIDTH;
-	private static int height=Config.SCREEN_HEIGHT;
-	//create graph from image using tile based division scheme
-	//input: empty graph
-	public static void createGraphFromImage(Graph graph, PApplet parent)
-	{
-		 PImage environment;
-		 environment = parent.loadImage("./src/environment/Environment.png");
-	   environment.loadPixels();
-	   int pixels[]=environment.pixels;
-		 int nodeIndex=0;
-		 //number of tile on each row
-		 int numberOfRow=(int) Math.floor((height/tileSize));
-		//number of tile on each column
-		 int numberOfColumn=(int) Math.floor((width/tileSize));
-		 //record map type
-		 int numberOfNodes=width*height;
-		 int [] node=new int[numberOfNodes];
-		 
-		 
-		 //quantization
-		 for(int y=0;y<height;y+=tileSize)
-		 {
-			 for(int x=0;x<width;x+=tileSize)
-			 {
-				 int pixel_index=(int) (Math.floor(x+tileSize/2)+Math.floor((y+tileSize/2))*width);
-				 int pixel_value=pixels[pixel_index]&0xFF;
+	public static void createGraphFromImage(Graph graph, PApplet parent) {
+		PImage environment;
+		environment = parent.loadImage("./src/environment/Environment.png");
+		environment.loadPixels();
+		int pixels[] = environment.pixels;
+		int nodeIndex = 0;
 
-				 //if the middle tile value is 0, then this node is closed
-				 if(pixel_value==0)
-				 {
-					 node[nodeIndex]=0;
-				 }
-				 //else open node, add connection(need to consider type)
-				 else
-				 {
-					 node[nodeIndex]=1;				 
-				 }
-				 nodeIndex++;
-			 }
-		 }
-		 //make connections
-		 for(nodeIndex=0;nodeIndex<numberOfNodes;nodeIndex++)
-		 {
-			 if(node[nodeIndex]==1)
-			 {
-				//connect left node
-				 if((nodeIndex-1)%numberOfColumn!=0 && nodeIndex-1>=0&&
-						 node[nodeIndex-1]==1)
-				 {
-    			 graph.addConnection(nodeIndex,nodeIndex-1,1);
-				 }
-				 //connect right node
-				 if((nodeIndex+1)%numberOfColumn!=0 && (nodeIndex+1)<numberOfNodes &&
-						 node[nodeIndex+1]==1)
-				 {
-					 graph.addConnection(nodeIndex,nodeIndex+1,1);
+		// detect the vertices of Dirichlet Domain
+		for (int i = 0; i < width; i++)
+			for (int j = 0; j < height; j++) {
+				// scanning in pixels[i*width+j]
+				if (pixels[i * width + j] == 0xFF0000) {
+					// RED pixel indicates Dirichlet Domain vertices
+					graph.setNodePos(nodeIndex, new Vec2D(i, j));
+					nodeIndex++;
+				}
+			}
 
-				 }
-				 //connect upper node
-				 if((nodeIndex-numberOfColumn)>=0 &&
-						 node[nodeIndex-numberOfColumn]==1)
-				 {
-					 graph.addConnection(nodeIndex,nodeIndex-numberOfColumn,1);
+		// create the edges in Dirichlet Domain
+		int V = graph.getNodeCount();
+		for (int i = 0; i < V; i++)
+			for (int j = i + 1; j < V; j++) {
+				Vec2D u = graph.getNodePos(i);
+				Vec2D v = graph.getNodePos(j);
+				if (detectAccessible(u, v, pixels)) {
+					int cost = (int) u.minus(v).getLength();
+					graph.addConnection(i, j, cost);
+					graph.addConnection(j, i, cost);
+				}
+			}
+	}
 
-				 }
-				 //connect lower node
-				 if((nodeIndex+numberOfColumn)<numberOfNodes &&
-						 node[nodeIndex+numberOfColumn]==1)
-				 {
-					 graph.addConnection(nodeIndex,nodeIndex+numberOfColumn,1);
-				 }
-			 }
-		 }
+	private static boolean detectAccessible(Vec2D u, Vec2D v, int[] pixels) {
+		int length = (int) u.minus(v).getLength() - 1;
+		for (int i = 0; i < length; i++) {
+			Vec2D t = v.minus(u).getUnitVec().multiply(i);
+			int x = (int) t.x;
+			int y = (int) t.y;
+			if (pixels[x * width + y] != 0xFFFFFF) // TODO modify access
+													// indicator
+				return false;
+		}
+		return true;
+	}
+}
+
+/*
+ * Tile Graph
+ */
+class World2 {
+
+	private static float tileSize = Config.TILE_SIZE;
+	private static int width = Config.SCREEN_WIDTH;
+	private static int height = Config.SCREEN_HEIGHT;
+
+	// create graph from image using tile based division scheme
+	// input: empty graph
+	public static void createGraphFromImage(Graph graph, PApplet parent) {
+		PImage environment;
+		environment = parent.loadImage("./src/environment/Environment.png");
+		environment.loadPixels();
+		int pixels[] = environment.pixels;
+		int nodeIndex = 0;
+		// number of tile on each row
+		int numberOfRow = (int) Math.floor((height / tileSize));
+		// number of tile on each column
+		int numberOfColumn = (int) Math.floor((width / tileSize));
+		// record map type
+		int numberOfNodes = width * height;
+		int[] node = new int[numberOfNodes];
+
+		// quantization
+		for (int y = 0; y < height; y += tileSize) {
+			for (int x = 0; x < width; x += tileSize) {
+				int pixel_index = (int) (Math.floor(x + tileSize / 2) + Math
+						.floor((y + tileSize / 2)) * width);
+				int pixel_value = pixels[pixel_index] & 0xFF;
+
+				// if the middle tile value is 0, then this node is closed
+				if (pixel_value == 0) {
+					node[nodeIndex] = 0;
+				}
+				// else open node, add connection(need to consider type)
+				else {
+					node[nodeIndex] = 1;
+				}
+				nodeIndex++;
+			}
+		}
+		// make connections
+		for (nodeIndex = 0; nodeIndex < numberOfNodes; nodeIndex++) {
+			if (node[nodeIndex] == 1) {
+				// connect left node
+				if ((nodeIndex - 1) % numberOfColumn != 0 && nodeIndex - 1 >= 0
+						&& node[nodeIndex - 1] == 1) {
+					graph.addConnection(nodeIndex, nodeIndex - 1, 1);
+				}
+				// connect right node
+				if ((nodeIndex + 1) % numberOfColumn != 0
+						&& (nodeIndex + 1) < numberOfNodes
+						&& node[nodeIndex + 1] == 1) {
+					graph.addConnection(nodeIndex, nodeIndex + 1, 1);
+
+				}
+				// connect upper node
+				if ((nodeIndex - numberOfColumn) >= 0
+						&& node[nodeIndex - numberOfColumn] == 1) {
+					graph.addConnection(nodeIndex, nodeIndex - numberOfColumn,
+							1);
+
+				}
+				// connect lower node
+				if ((nodeIndex + numberOfColumn) < numberOfNodes
+						&& node[nodeIndex + numberOfColumn] == 1) {
+					graph.addConnection(nodeIndex, nodeIndex + numberOfColumn,
+							1);
+				}
+			}
+		}
 	}
 
 }
--- src/pathfinding/Graph.java
+++ src/pathfinding/Graph.java
@@ -1,84 +1,113 @@
 package pathfinding;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
 import main.Config;
+import physics.Vec2D;
 
 public class Graph {
-	
+
+	private HashMap<Integer, Vec2D> nodePos = null;
 	private HashMap<Integer, List<Connection>> connectionLists = null;
 	private int maxId = -1;
 	private int screenWidth = 0;
-	
-	public Graph() {	
+
+	public Graph() {
+		nodePos = new HashMap<Integer, Vec2D>();
 		connectionLists = new HashMap<Integer, List<Connection>>();
 		screenWidth = Config.SCREEN_WIDTH;
 	}
-	
+
+	public void setNodePos(int nodeIndex, Vec2D pos) {
+		if (nodePos.containsKey(nodeIndex))
+			nodePos.replace(nodeIndex, pos);
+		else
+			nodePos.put(nodeIndex, pos);
+	}
+
+	public Vec2D getNodePos(int nodeIndex) {
+		return nodePos.get((Object) nodeIndex);
+	}
+
+	public int getNodeCount() {
+		return nodePos.keySet().size();
+	}
+
 	public void addConnection(int from, int to, int cost) {
 		Connection conn = new Connection(from, to, cost);
-		
-		if(connectionLists.containsKey(from) == false) {
+
+		if (connectionLists.containsKey(from) == false) {
 			List<Connection> list = new ArrayList<Connection>();
 			list.add(conn);
 			connectionLists.put(from, list);
-		}else{
+		} else {
 			connectionLists.get(from).add(conn);
 		}
 	}
-	
-	
-	//return list of outgoing connections from given node
+
+	// return list of outgoing connections from given node
 	public List<Connection> getConnections(int fromNode) {
 		List<Connection> list = new ArrayList<Connection>();
-		if(connectionLists.containsKey(fromNode))
+		if (connectionLists.containsKey(fromNode))
 			list = connectionLists.get(fromNode);
 		return list;
 	}
-	
-	//get number of vertices in graph
+
+	// get number of vertices in graph
 	public int order() {
 		return connectionLists.size();
 	}
-	
-	//get number of edges in graph
+
+	// get number of edges in graph
 	public int size() {
 		int size = 0;
-		for(Integer key : connectionLists.keySet()) {
+		for (Integer key : connectionLists.keySet()) {
 			size += connectionLists.get(key).size();
 		}
 		return size;
 	}
-	
-	//get max node id
+
+	// get max node id
 	public int maxId() {
-		if(maxId == -1) {
-			for(Integer key : connectionLists.keySet())
-				if(key > maxId) maxId = key;
+		if (maxId == -1) {
+			for (Integer key : connectionLists.keySet())
+				if (key > maxId)
+					maxId = key;
 		}
 		return maxId;
 	}
-	
+
 	public int[] getRowColFromNode(int node) {
 		int[] pixel = new int[2];
 		pixel[0] = node % screenWidth;
 		pixel[1] = node / screenWidth + 1;
-		if(node % screenWidth == 0) {
+		if (node % screenWidth == 0) {
 			pixel[0] = screenWidth;
 			pixel[1]--;
 		}
 		return pixel;
 	}
-	
-	//debug, print adjacency list of graph
+
+	// debug, print adjacency list of graph
 	public void printAdjacencyList() {
-		for(Integer key:connectionLists.keySet()) {
+		for (Integer key : connectionLists.keySet()) {
 			System.out.print(key + ":");
 			List<Connection> vals = connectionLists.get(key);
-			for(int i=0;i<vals.size();i++) {
+			for (int i = 0; i < vals.size(); i++) {
 				System.out.print(vals.get(i).getToNode() + ",");
 			}
 			System.out.println();
 		}
 	}
 
+	public static void main(String[] args) {
+		Graph g = new Graph();
+		g.setNodePos(0, new Vec2D(312, 21));
+		g.setNodePos(1, new Vec2D(12, 111));
+		g.setNodePos(1, new Vec2D(1, 11));
+		System.out.println(g.getNodePos(5));
+		System.out.println(g.getNodeCount());
+	}
 }
