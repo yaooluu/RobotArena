640d34717458113357e95ffdc9d9fccb5b5c704c
Yao <Yao@WIN-G7OPLOEHB8C>
2015-03-29 17:14:53 -0400

    Implemented PathLibrary

--- src/pathfinding/PathLibrary.java
+++ src/pathfinding/PathLibrary.java
@@ -1,12 +1,22 @@
 package pathfinding;
 
+import java.util.List;
+
+import environment.World;
+import main.Main;
 import physics.*;
 
 public class PathLibrary {
 
 	public static Vec2D getNextTarget(Vec2D pos, Vec2D targetPos) {
-		// TODO Auto-generated method stub
-		return null;
+		Graph graph = Main.getGraph();
+		int start = World.quantize(pos);
+		int end = World.quantize(targetPos);
+		
+		if(start == end) return targetPos;
+
+		List<Integer> path = PathFinding.AStar(graph, start, end);
+		return graph.getNodePos(path.get(0));
 	}
 
 }
