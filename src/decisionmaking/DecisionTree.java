9fcf7717531e6b2efe2cc8ad28bcde81c29ba899
Jianfeng <ginfungchan@gmail.com>
2015-04-03 16:51:33 -0400

    add DT action

--- src/decisionmaking/DecisionTree.java
+++ src/decisionmaking/DecisionTree.java
@@ -1,11 +1,63 @@
 package decisionmaking;
 
 import main.Config;
+import behavior.Attack;
+import behavior.Buff;
+import behavior.Evade;
+import behavior.Trace;
+import behavior.Wander;
 import environment.Boid;
 
 public class DecisionTree {
 
-	public static String makeDecison(Boid b, int parent) {
+	public static void PerformDecision(Boid b){
+		Boid enemy = b.getVisibleEnemy() != null ? b.getVisibleEnemy() : b
+				.getAudibleEnemy();
+		
+		switch (DecisionTree.makeDecision(b,1)) {
+		case "attack":
+			Attack.goAttack(b, enemy);
+			break;
+
+		case "evade":
+			Evade.evade(b, enemy);
+			break;
+
+		case "redbuff":
+			Buff.goBuff(b, "red");
+			break;
+			
+		case "bluebuff":
+			Buff.goBuff(b, "blue");
+			break;
+			
+		case "trace":
+			Trace.trace(b, enemy);
+			break;
+			
+		case "wander":
+			Wander.wander(b);
+			break;
+			
+		case "lure":
+			//TODO
+			break;
+			
+		case "ultimate":
+			//TODO
+			break;
+
+		case "guard":
+			//TODO
+			break;
+			
+		case "hide":
+			//TODO
+			break;
+		}
+	}
+	
+	private static String makeDecision(Boid b, int parent) {
 		int index = parent;
 		do {
 			parent = index;
