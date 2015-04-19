b5dc3310172ad35412afe97d6b3b4e8a1df891eb
Jianfeng <ginfungchan@gmail.com>
2015-04-18 21:20:03 -0400

    ini

--- src/main/Main.java
+++ src/main/Main.java
@@ -57,13 +57,13 @@ public class Main extends PApplet {
 			}			
 		}//*/	
 
-		//boids.add(new Boid(100, 150, 90, 0, Config.BOID_TYPE.scout, 1));
+		boids.add(new Boid(100, 150, 90, 0, Config.BOID_TYPE.scout, 1));
 		boids.add(new Boid(100, 250, 180, 0, Config.BOID_TYPE.soldier, 2));
-		//boids.add(new Boid(100, 350, 90, 0, Config.BOID_TYPE.tank, 3));
+		boids.add(new Boid(100, 350, 90, 0, Config.BOID_TYPE.tank, 3));
 		
-		//boids.add(new Boid(700, 200, 270, 1, Config.BOID_TYPE.scout, 4));
+		boids.add(new Boid(700, 200, 270, 1, Config.BOID_TYPE.scout, 4));
 		boids.add(new Boid(700, 300, 270, 1, Config.BOID_TYPE.soldier, 5));
-		//boids.add(new Boid(700, 400, 270, 1, Config.BOID_TYPE.tank, 6));
+		boids.add(new Boid(700, 400, 270, 1, Config.BOID_TYPE.tank, 6));
 		player=new Player(boids.get(0));
 		SafetyEval.debug();
 		CostEval.debug();
